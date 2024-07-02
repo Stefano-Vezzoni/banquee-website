d1be2e2d5e59dc6c937cd7a705c6c774cfb569b1
unknown <stefanov.santos@gmail.com>
2024-07-02 11:43:41 -0300

    (Compare) Hero finished.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { Features } from "./pages/features/Features";
+import { Compare } from "./pages/compare/Compare";
 
 export function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/features" element={<Features />} />
+            <Route path="/compare" element={<Compare />} />
         </Routes>
     );
 }
---
+++ src/pages/compare/Compare.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/Header";
+import { Hero } from "./components/Hero";
+
+export function Compare() {
+    return (
+        <div className="text-xl font-medium">
+            <Header />
+
+            <Hero />
+        </div>
+    );
+}
